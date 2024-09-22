2083b9a79d5c4d6dd990a91360a10d0e536a23af
mengxx <290942762@qq.com>
2024-09-22 23:06:09 +0800

    fix: bug

--- src/cores/middleware/RemoteAuthenticate.php
+++ src/cores/middleware/RemoteAuthenticate.php
@@ -2,6 +2,7 @@
 
 namespace cores\middleware;
 
+use cores\exception\DenyException;
 use cores\exception\UnAuthenticatedException;
 use cores\Request;
 use GuzzleHttp\Client;
@@ -12,6 +13,7 @@ class RemoteAuthenticate
 
     /**
      * @throws UnAuthenticatedException
+     * @throws DenyException
      */
     public function handle(Request $request, \Closure $next)
     {
@@ -19,38 +21,42 @@ class RemoteAuthenticate
         $remoteAuthUrl = config('app.remote_aut_address');
 
         if (empty($remoteAuthUrl)) {
-            throw new UnAuthenticatedException('Authentication service not configured');
+            throw new DenyException();
         }
 
         // 创建 Guzzle HTTP 客户端
-        $client = new Client();
+        $client = new Client(['base_uri' => $remoteAuthUrl]);
         try {
             $path = $request->baseUrl();
             // 转发当前请求到远程用户中心 添加proxy前缀
-            $remoteAuthUrl = printf("%s/proxy/%s", $remoteAuthUrl, $path);
+            $remoteAuthUrl = sprintf("proxy%s", $path);
             $response = $client->request($request->method(), $remoteAuthUrl, [
                 'headers' => $request->header(), // 转发原始请求的 headers
-                'query' => $request->query(),   // 转发原始请求的 query 参数
-                'json' => $request->input(),        // 转发原始请求的 body 参数 (json 数据)
+                'json' => $request->param(),        // 转发原始请求的 body 参数 (json 数据)
                 'timeout' => 5,                        // 设置请求超时
             ]);
 
             // 处理远程服务器返回的响应
             if ($response->getStatusCode() !== 200) {
-                throw new UnAuthenticatedException('Authentication failed');
+                throw new DenyException();
             }
 
             // 根据需求处理响应的结果
-            $userData = json_decode($response->getBody()->getContents(), true);
+            $res = json_decode($response->getBody()->getContents(), true);
+            if ($res['code'] !== 200) {
+                throw new DenyException($res['message']);
+            }
 
             // 你可以根据远程服务的响应进一步处理，比如检查用户身份是否有效
-            $request->setUserResolver(function () use ($userData) {
-                return $userData;
+            $request->setUserResolver(function () use ($res) {
+                return $res['data'];
             });
+
+            $request->header("Saasid", $res['data']['saas_id']);
 
         } catch (GuzzleException $e) {
             // 捕获 Guzzle 请求异常，并处理错误情况
-            throw new UnAuthenticatedException('Error communicating with authentication service: ' . $e->getMessage());
+            throw new DenyException('Error communicating with authentication service: ' . $e->getMessage());
         }
 
         // 继续执行下一个中间件
