db780bf1747f63ede06bdb7860ed95036e720fc4
mengxx <290942762@qq.com>
2025-01-17 11:45:17 +0800

    fix: bug

--- src/cores/helper/functions.php
+++ src/cores/helper/functions.php
@@ -18,3 +18,25 @@ if (!function_exists('getCurrentUserId')) {
         return 0;
     }
 }
+
+
+if (!function_exists('encrypt')) {
+    function encrypt($data): string
+    {
+        $key = "$$3456789001235467891234567890KK"; // 32 字节 (256 位) 密钥
+        // 使用 openssl_encrypt 函数进行加密
+        $ciphertext = openssl_encrypt($data, 'AES-256-ECB', $key, OPENSSL_RAW_DATA);
+
+        return base64_encode($ciphertext);
+    }
+}
+
+if (!function_exists('decrypt')) {
+    function decrypt($data): string
+    {
+        $key = "$$3456789001235467891234567890KK"; // 32 字节 (256 位) 密钥
+        $ciphertext = base64_decode($data);
+        // 使用 openssl_encrypt 函数进行加密
+        return openssl_decrypt($ciphertext, 'AES-256-ECB', $key, OPENSSL_RAW_DATA);
+    }
+}
